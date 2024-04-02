fc8e99d1346c36c3e5ef381b9d2bc017f38154be
Bholanath <bholanathbarik9748@gmail.com>
2024-04-02 23:19:38 +0530

    Initial Commit

---
+++ JPApex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApex
+//
+//  Created by Bholanath Barik on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApex/JPApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexApp.swift
+//  JPApex
+//
+//  Created by Bholanath Barik on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
